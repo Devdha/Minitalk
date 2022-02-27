bc7f68253d98b79b42e9a954473efc1895407b7b
p0tt3r-iOS <france998811@gmail.com>
2022-02-27 21:57:09 +0900

    almost done

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: dha <dha@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 19:42:01 by dha               #+#    #+#             */
-/*   Updated: 2022/02/27 20:48:01 by dha              ###   ########seoul.kr  */
+/*   Updated: 2022/02/27 21:54:00 by dha              ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,49 +16,26 @@ static void	init_sigact(void);
 
 static void	recv_msg(int sig, siginfo_t *info, void *context)
 {
-	static unsigned char	*ch;
+	static unsigned char	ch;
 	static int				recv;
-	static int				cnt = 1;
-	static int				ch_bytes;
 
-	if (!ch)
-		ch = malloc(sizeof(char) * 5);
-	if (!ch)
-		exit(EXIT_FAILURE);
 	recv++;
 	if (sig == SIGUSR1)
-	{
-		ch[cnt - 1] &= ~(1 << (8 - recv));
-	}
+		ch &= ~(1 << (8 - recv));
 	else if (sig == SIGUSR2)
-	{
-		if (recv == 1)
-			cnt++;
-		ch[cnt - 1] |= (1 << (8 - recv));
-		if (cnt == 2 && (recv >= 1 && recv <= 4))
-			ch_bytes++;
-	}
+		ch |= (1 << (8 - recv));
 	if (recv == 8)
 	{
-		if (!ch[cnt - 1])
+		if (!ch)
 		{
 			write(1, "\n", 1);
 			kill(info->si_pid, SIGUSR2);
 			init_sigact();
 			ch = 0;
 			recv = 0;
-			ch_bytes = 0;
-			cnt = 1;
 			return ;
 		}
-		if (!(ch_bytes - cnt + 1))
-		{
-			write(1, ch, cnt);
-			cnt = 1;
-			ch_bytes = 0;
-			free(ch);
-			ch = NULL;
-		}
+		write(1, &ch, 1);
 		recv = 0;
 	}
 	kill(info->si_pid, SIGUSR1);
