3f27d30fcd92ba46c4718173a6ce116b168b2793
p0tt3r-iOS <france998811@gmail.com>
2022-06-17 10:11:06 +0900

    fix: replace sigaction to signal

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: dha <dha@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 19:42:02 by dha               #+#    #+#             */
-/*   Updated: 2022/02/28 01:33:12 by dha              ###   ########seoul.kr  */
+/*   Updated: 2022/06/17 10:08:36 by dha              ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,16 +30,14 @@ static int	getbit(void)
 	return (0);
 }
 
-static void	send_msg(int sig, siginfo_t *info, void *context)
+static void	send_msg(int signo)
 {
 	int	bit;
 
-	(void) info;
-	(void) context;
 	bit = getbit();
-	if (sig == SIGUSR2)
+	if (signo == SIGUSR2)
 		exit(EXIT_SUCCESS);
-	if (sig == SIGUSR1)
+	if (signo == SIGUSR1)
 	{
 		if (!bit)
 			kill(g_info.server_pid, SIGUSR1);
@@ -48,21 +46,16 @@ static void	send_msg(int sig, siginfo_t *info, void *context)
 	}
 }
 
-static void	check_connect(int sig, siginfo_t *info, void *context)
+static void	check_connect(int signo)
 {
-	t_sigact	sigact;
-	int			bit;
+	int	bit;
 
-	(void) sig;
-	(void) info;
-	(void) context;
+	(void) signo;
 	ft_putstr_fd("Connect to server(PID: ", 1);
 	ft_putnbr_fd(g_info.server_pid, 1);
 	ft_putendl_fd(") successfully", 1);
-	sigact.sa_flags = SA_SIGINFO;
-	sigact.sa_sigaction = send_msg;
-	if (sigaction(SIGUSR1, &sigact, NULL) == -1
-		|| sigaction(SIGUSR2, &sigact, NULL) == -1)
+	if (signal(SIGUSR1, send_msg) == SIG_ERR
+		|| signal(SIGUSR2, send_msg) == SIG_ERR)
 		error_exit("[Error] Sigaction failed its work.");
 	bit = getbit();
 	if (!bit)
@@ -73,11 +66,7 @@ static void	check_connect(int sig, siginfo_t *info, void *context)
 
 void	init_sigact(void)
 {
-	t_sigact	sigact;
-
-	sigact.sa_flags = SA_SIGINFO;
-	sigact.sa_sigaction = check_connect;
-	if (sigaction(SIGUSR1, &sigact, NULL) == -1)
+	if (signal(SIGUSR1, check_connect) == SIG_ERR)
 		error_exit("[Error] Sigaction failed its work.");
 }
 
